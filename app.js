c550a7737bccb5c731ae3d615fd0b63b76887951
Alfie Phillips <74322635+AlfiePhillips@users.noreply.github.com>
2021-03-22 17:32:41 +0000

    setup for mongo db

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
 
 // Load config
 
 dotenv.config({ path: "./config/config.env" });
 
+// Connect Database
+
+connectDB();
+
 // Initialize app
 
 const app = express();
 
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Handlebars
+
+app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
 const PORT = process.env.PORT || 5000;
-const HOST = process.env.HOST || "localhost";
 
 // Listen for requests
 
 app.listen(PORT, (HOST) => {
-  console.log(`Server starting on ${HOST}:${PORT}`);
+  console.log(
+    `Server starting on ${process.env.NODE_ENV} mode on port ${PORT}`
+  );
 });
