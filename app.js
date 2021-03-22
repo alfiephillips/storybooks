2c13c411b79a69e85c10c9f9b377c39ef851a3a8
Alfie Phillips <74322635+AlfiePhillips@users.noreply.github.com>
2021-03-22 17:44:49 +0000

    added base route

--- app.js
+++ app.js
@@ -25,6 +25,9 @@ if (process.env.NODE_ENV === "development") {
 app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".hbs");
 
+//Routes
+app.use("/", require("./routes/index"));
+
 const PORT = process.env.PORT || 5000;
 
 // Listen for requests
