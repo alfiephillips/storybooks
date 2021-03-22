75004ff6650e3d76453ad56cbcc6bd9fdbed9e46
Alfie Phillips <74322635+AlfiePhillips@users.noreply.github.com>
2021-03-22 22:31:46 +0000

    fixed node module bug and started stories

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const path = require("path");
 const express = require("express");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const connectDB = require("./config/db");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
+const methodOverride = require("method-override");
 const passport = require("passport");
 const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const connectDB = require("./config/db");
+
 // Load config
 
 dotenv.config({ path: "./config/config.env" });
@@ -32,12 +36,12 @@ app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".hbs");
 
 // Sessions
-
 app.use(
   session({
-    secret: "secret",
+    secret: "keyboard cat",
     resave: false,
     saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
   })
 );
 
