480871fd719710eea63d7e378f330a2a817729f0
Alfie Phillips <74322635+AlfiePhillips@users.noreply.github.com>
2021-03-23 20:46:51 +0000

    adding stories function

---
+++ routes/stories.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const { ensureAuth } = require("../middleware/auth");
+
+const Story = require("../models/Story");
+
+// @desc Show the add page
+// @route GET /stories/add
+
+router.get("/add", ensureAuth, (req, res) => {
+  res.render("stories/add");
+});
+
+module.exports = router;
