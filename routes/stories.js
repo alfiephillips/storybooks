1233de519816b84bfec1ce8ad111376f72bf965b
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2021-04-28 21:14:25 +0100

    getting stories

--- routes/stories.js
+++ routes/stories.js
@@ -1,36 +1,44 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { ensureAuth } = require("../middleware/auth");
+const { ensureAuth } = require('../middleware/auth');
+const Story = require('../models/Story');
 
-const Story = require("../models/Story");
+// @desc    Show add page
+// @route   GET /stories/add
+router.get('/add', ensureAuth, (req, res) => {
+  res.render('stories/add')
+})
 
-// @desc Show public stories
-// @route GET /stories
-
-router.get("/", ensureAuth, (req, res) => {
-  res.render("/stories");
-});
-
-// @desc Show the add page
-// @route GET /stories/add
-
-router.get("/add", ensureAuth, (req, res) => {
-  res.render("stories/add");
-});
-
-// @desc Process add form
-// @route POST /stories
-
-router.post("/", ensureAuth, async (req, res, next) => {
+// @desc    Process add form
+// @route   POST /stories
+router.post('/', ensureAuth, async (req, res) => {
   try {
-    req.body.user = req.user.id;
-    await Story.create(req.body);
-    res.redirect("/dashboard");
-    next();
+    req.body.user = req.user.id
+    await Story.create(req.body)
+    res.redirect('/dashboard')
   } catch (err) {
-    console.log(err);
-    res.render("error/500");
+    console.error(err)
+    res.render('error/500')
   }
-});
+})
+
+// @desc    Show all stories
+// @route   GET /stories
+router.get('/', ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ status: 'public' })
+      .populate('user')
+      .sort({ createdAt: 'desc' })
+      .lean()
+
+    res.render('stories/index', {
+      stories,
+    })
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
+})
+
 
 module.exports = router;
